533ddcb52528167d89f8fae69767e7047fcd90bb
Matt Dickenson <mcdickenson@gmail.com>
2013-09-17 14:03:42 -0400

    first draft answer to problem 1

--- homework/hw3/cs571-hw3.R
+++ homework/hw3/cs571-hw3.R
@@ -38,4 +38,47 @@ linreg = function(X, Y){
 model = linreg(X, Y)
 model$beta #=> 3.0117879, 0.7832938
 
+# C 
+ridgereg = function(X, Y, sigma.sq=1, tau.sq=1){
+	lambda = sigma.sq / tau.sq 
+	D = ncol(X)
+	lambda.id = lambda * diag(D)
+	print(lambda.id)
+	X.prime.X = t(X) %*% X
+	lambda.X.prime.X.inverse = solve(lambda.id + X.prime.X)
+	X.prime.Y = t(X) %*% Y
+	beta.hat = lambda.X.prime.X.inverse %*% X.prime.Y
+	return(beta.hat)
+}
+beta.ridge = ridgereg(X, Y)
+beta.ridge #=> 2.9972982, 0.7833412
 
+# D 
+# a
+y.hat = X %*% beta.hat
+RSSa = sum((Y - y.hat)^2)
+RSSa #=> 181767.9
+# b 
+model.yhat = X %*% model$beta
+RSSb = sum((Y - model.yhat)^2)
+RSSb #=> 181767.9
+# c
+ridge.yhat = X %*% beta.ridge
+RSSc = sum((Y - ridge.yhat)^2)
+RSSc #=> 181768
+
+# E
+X.test = as.matrix(lindata[1001:1100 , 1:2])
+Y.test = as.matrix(lindata[1001:1100 , 3])
+# a
+y.hat = X.test %*% beta.hat
+RSSa = sum((Y.test - y.hat)^2)
+RSSa #=> 16963.35
+# b 
+model.yhat = X.test %*% model$beta
+RSSb = sum((Y.test - model.yhat)^2)
+RSSb #=> 16963.35
+# c
+ridge.yhat = X.test %*% beta.ridge
+RSSc = sum((Y.test - ridge.yhat)^2)
+RSSc #=> 16963.59
