25684bbe4996b28f8e3997f0d28eb9a363009b76
Matt Dickenson <mcdickenson@gmail.com>
2013-10-21 12:40:40 -0400

    start hw5.1a

--- homework/hw5/cs571-hw5.R
+++ homework/hw5/cs571-hw5.R
@@ -3,6 +3,38 @@
 # Homework 5
 
 setwd('~/github/cs571/homework/hw5')
+
+# Problem 1
+# a
+set.seed(123)
+n = p = 100
+k = 3
+phi = c(0.2, 2, 10)
+z = matrix(rnorm(n*k, 0, 1), nrow=k, ncol=n)
+L = matrix(rnorm(p*k, 0, 1), nrow=p, ncol=k)
+epsilon1 = matrix(rnorm(p*n, 0, phi[1]), nrow=p, ncol=n)
+epsilon2 = matrix(rnorm(p*n, 0, phi[2]), nrow=p, ncol=n)
+epsilon3 = matrix(rnorm(p*n, 0, phi[3]), nrow=p, ncol=n)
+X1 = L%*%z + epsilon1
+X2 = L%*%z + epsilon2
+X3 = L%*%z + epsilon3
+
+eigen1 = eigen(cov(X1))$values
+eigen2 = eigen(cov(X2))$values
+eigen3 = eigen(cov(X3))$values
+
+pdf("1a.pdf")
+par(mfrow=c(1,3))
+plot(density(eigen1), main="X1")
+plot(density(eigen2), main="X2")
+plot(density(eigen3), main="X3")
+dev.off()
+
+# b
+
+
+
+# Problem 2
 train = read.table("train_data_gp.txt", header=FALSE)
 
 
