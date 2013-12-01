d1ab71bb02e9e565ad6a6415ea8ccaac2c668bdf
Matt Dickenson <mcdickenson@gmail.com>
2013-12-01 18:06:12 -0500

    clean up map plotter

--- poster/plot-maps.R
+++ poster/plot-maps.R
@@ -1,8 +1,5 @@
 #!/usr/bin/Rscript
 
-install.packages('rworldmap')
-
-
 # set up workspace
 rm(list=ls())
 setwd('~/github/gdelt-to-mids')
@@ -74,29 +71,8 @@ mid = summaryBy(mid ~ ccode, data=combos, FUN=mean)
 head(mid)
 mid$iso3 = countrycode(mid$ccode, "cown", "iso3c")
 
-# subset = icews[icews$year==2010 & icews$month==1, c("ccode", "year", "month")]
-# subset$mid.mean = 0 
-# for(i in 1:nrow(subset)){
-# 	code = subset[i, "ccode"]
-# 	midmean = mean.mid[which(mid$ccode==code)[1], "mid.mean"]
-# 	subset$mid.mean[i] = ifelse(is.na(midmean), 0, midmean)
-# }
-
-# head(subset)
-# subset$iso3 = countrycode(subset$ccode, "cown", "iso3c")
-
-# mult = 1
-# colorscheme = rgb(red=1, blue=1-(subset$mid.mean*mult), green=1-(subset$mid.mean*mult) )
-# props = c(0.10*mult, 0.50*mult, 0.90*mult)
-# legendcolors = rgb(red=1, blue=1-props, green=1-props )
-
 pathGraphics = "~/github/cs571/graphics"
 setwd(pathGraphics)
-
-# tikz('midmap.tex', standAlone=TRUE, width=4.5, height=5)
-# plot(icewsmap, col=colorscheme)
-# legend("right", legend=c("10\\%", "50\\%", "90\\%"), cex=0.5, col=legendcolors, pch=15)
-# dev.off()
 
 
 data(countryExData)
@@ -109,4 +85,3 @@ mapCountryData(sPDF, nameColumnToPlot="mid.mean", mapTitle="",
 	numCats=10)
 dev.off()
 
-?mapCountryData
