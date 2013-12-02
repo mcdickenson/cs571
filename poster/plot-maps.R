99033c91a081cb238d4fcf9ecc9544e2d755964d
Matt Dickenson <mcdickenson@gmail.com>
2013-12-01 20:27:20 -0500

    add maps of false positives and false negatives

--- poster/plot-maps.R
+++ poster/plot-maps.R
@@ -27,8 +27,8 @@ library(partykit) 		# Convert rpart object to BinaryTree
 setwd(pathData)
 
 load('output6.rda')
-# load("train-data.rda")
-# load("test-data.rda")
+load("train-data.rda")
+load("test-data.rda")
 dim(train)
 dim(test)
 
@@ -82,6 +82,186 @@ tikz('midmap.tex', standAlone=FALSE, width=4.5, height=3)
 mapDevice(device="tikz output")
 # mapDevice()
 mapCountryData(sPDF, nameColumnToPlot="mid.mean", mapTitle="",
-	numCats=10)
+	catMethod='pretty')
 dev.off()
+
+
+####
+# map accuracy in training set 
+ls()
+names(train)
+countries = sort(unique(c(unique(train$country_1), unique(train$country_2))))
+mths = seq(min(train$date), max(train$date), by='month')
+
+# combos = expand.grid(first=countries, second=mths)
+# colnames(combos) = c("country", "date")
+
+combos = as.data.frame(countries)
+head(combos)
+colnames(combos) = c("country")
+
+combos$tp = 0
+combos$tn = 0
+combos$fp = 0 
+combos$fn = 0 
+head(combos)
+
+yhat = predict(tree_mine, type='class')
+yobs = train$hostile4
+
+for(i in 1:nrow(train)){
+	if(i%%1000==0){print(i)}
+	a = train$country_1[i]
+	b = train$country_2[i]
+	est = yhat[i]
+	obs = yobs[i]
+	var = ""
+	if(obs==0 & est==0){
+		var = "tn"
+	} else if(obs==0 & est==1){
+		var = "fp"
+	} else if(obs==1 & est==0){
+		var = "fn"
+	} else {
+		var = "tp"
+	}
+
+	combos[which(combos$country==a), var] = combos[which(combos$country==a), var] + 1
+	combos[which(combos$country==b), var] = combos[which(combos$country==b), var] + 1	
+}
+
+combos$total = combos$tp + combos$tn + combos$fp + combos$fn 
+combos$tp.perc = combos$tp / combos$total
+combos$tn.perc = combos$tn / combos$total
+combos$fp.perc = combos$fp / combos$total
+combos$fn.perc = combos$fn / combos$total
+
+head(combos)
+tail(combos)
+
+pathGraphics = "~/github/cs571/graphics"
+setwd(pathGraphics)
+
+summary(combos$fn.perc)
+combos[order(combos$fn.perc),]
+names(combos)
+combos[which(combos$country=="SRB"), 'fn.perc'] = 0.049999
+
+data(countryExData)
+sPDF = joinCountryData2Map( combos, joinCode="ISO3", nameJoinColumn="country")
+
+tikz('train-false-positives.tex', standAlone=FALSE, width=4.5, height=3)
+mapDevice(device="tikz output")
+# mapDevice()
+mapCountryData(sPDF, nameColumnToPlot="fp.perc", mapTitle="", catMethod=seq(0, 0.005, length.out=10),
+	addLegend=FALSE)
+dev.off()
+
+tikz('train-false-negatives.tex', standAlone=FALSE, width=4.5, height=3)
+mapDevice(device="tikz output")
+# mapDevice()
+mapCountryData(sPDF, nameColumnToPlot="fn.perc", mapTitle="", catMethod=seq(0, 0.12, length.out=10),
+	addLegend=FALSE)
+dev.off()
+
+
+####
+# map accuracy in test set 
+
+countries = sort(unique(c(unique(test$country_1), unique(test$country_2))))
+mths = seq(min(test$date), max(test$date), by='month')
+
+combos2 = as.data.frame(countries)
+head(combos2)
+colnames(combos2) = c("country")
+
+combos2$tp = 0
+combos2$tn = 0
+combos2$fp = 0 
+combos2$fn = 0 
+head(combos2)
+
+yhat_test = as.numeric(predict(tree_mine_pruned, type='class', newdata=test)) - 1
+yobs_test = test$hostile4
+
+for(i in 1:nrow(test)){
+	if(i%%1000==0){print(i)}
+	a = test$country_1[i]
+	b = test$country_2[i]
+	est = yhat_test[i]
+	obs = yobs_test[i]
+	var = ""
+	if(obs==0 & est==0){
+		var = "tn"
+	} else if(obs==0 & est==1){
+		var = "fp"
+	} else if(obs==1 & est==0){
+		var = "fn"
+	} else {
+		var = "tp"
+	}
+
+	combos2[which(combos2$country==a), var] = combos2[which(combos2$country==a), var] + 1
+	combos2[which(combos2$country==b), var] = combos2[which(combos2$country==b), var] + 1	
+}
+
+combos2$total = combos2$tp + combos2$tn + combos2$fp + combos2$fn 
+combos2$tp.perc = combos2$tp / combos2$total
+combos2$tn.perc = combos2$tn / combos2$total
+combos2$fp.perc = combos2$fp / combos2$total
+combos2$fn.perc = combos2$fn / combos2$total
+
+head(combos2)
+tail(combos2)
+
+# pathGraphics = "~/github/cs571/graphics"
+# setwd(pathGraphics)
+
+combos2[order(combos2$fp.perc),]
+combos2[which(combos2$country=="PSE"), 'fp.perc'] = 0.0049999
+
+data(countryExData)
+sPDF2 = joinCountryData2Map( combos2, joinCode="ISO3", nameJoinColumn="country")
+
+tikz('test-false-positives.tex', standAlone=FALSE, width=4.5, height=3)
+mapDevice(device="tikz output")
+# mapDevice()
+mapCountryData(sPDF2, nameColumnToPlot="fp.perc", mapTitle="", catMethod=seq(0, 0.005, length.out=10) )
+dev.off()
+
+
+tikz('test-false-negatives.tex', standAlone=FALSE, width=4.5, height=3)
+# mapDevice(device="tikz output")
+mapDevice()
+par(mfrow=c(2,1))
+mapCountryData(sPDF2, nameColumnToPlot="fn.perc", mapTitle="", catMethod=seq(0, 0.12, length.out=10))
+# dev.off()
+
+
+tikz('false-positives.tex', standAlone=FALSE, width=4.5, height=6)
+mapDevice(device="tikz output")
+par(mfrow=c(2,1))
+par(oma=c(-1,-1,-1,-1))
+# mapDevice()
+mapCountryData(sPDF, nameColumnToPlot="fp.perc", mapTitle="False Positives", catMethod=seq(0, 0.005, length.out=10),
+	addLegend=FALSE)
+mapCountryData(sPDF2, nameColumnToPlot="fp.perc", mapTitle="", catMethod=seq(0, 0.005, length.out=10) )
+dev.off()
+
+# getwd()
+tikz('diagnostics.tex', standAlone=TRUE, width=4.5, height=8.5)
+mapDevice(device="tikz output")
+# mapDevice()
+par(mfrow=c(4,1))
+par(mar=c(0,0,2,2))
+mapCountryData(sPDF, nameColumnToPlot="fp.perc", mapTitle="False Positives", catMethod=seq(0, 0.005, length.out=10),
+	addLegend=TRUE)
+mapCountryData(sPDF2, nameColumnToPlot="fp.perc", mapTitle="", catMethod=seq(0, 0.005, length.out=10) ,
+	addLegend=FALSE)
+mapCountryData(sPDF2, nameColumnToPlot="fn.perc", mapTitle="False Negatives", catMethod=seq(0, 0.12, length.out=10),
+	addLegend=TRUE)
+mapCountryData(sPDF2, nameColumnToPlot="fn.perc", mapTitle="", catMethod=seq(0, 0.12, length.out=10),
+	addLegend=FALSE)
+dev.off()
+
 
